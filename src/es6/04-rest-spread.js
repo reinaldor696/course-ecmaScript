b58141309f1e188d81086d8b523099948a0289af
Reinaldo Romero <reinaldoromero696@gmail.com>
2023-07-18 14:02:22 -0400

    class 4 spread operator and rest paramers es6

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -17,3 +17,30 @@ let user = {
 let {username, age} = user;
 
 console.log(username, age);
+
+//spread operator = operador de propagacion
+
+//the spread operator (...) it's used for add a new information to an object
+// spread operator brings all it have into an object
+let person = {
+    name: 'Reinaldo',
+    age: 29,
+};
+
+let country = 'US';
+
+let data = {id: 1, ...person, country};
+
+console.log(data);
+
+//rest paramers
+
+//(...values) agarra todo los parametros que le enviamos y los agrega a este elemento de value
+//NOTA:es recomendable solo asignar menos de 4 valores por que el codigo no puede recibir tantos parametros ya que puede causar mucha confusion
+function sum(num, ...values){
+    console.log(values);
+    console.log(num + values[0]);
+    return num + values[0];
+};
+
+sum(1, 1, 2, 3);
