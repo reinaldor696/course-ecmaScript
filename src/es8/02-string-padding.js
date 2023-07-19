a606633a0629d463ec2a0b9e26cc6026252c78e3
Reinaldo Romero <reinaldoromero696@gmail.com>
2023-07-19 15:42:10 -0400

    class 13 string padding and trailing commas es8

---
+++ src/es8/02-string-padding.js
@@ -0,0 +1,10 @@
+//actualizacion javascript junio 2017
+
+//metodo padStart nos permite agregar donde va a iniciar y que queremos agregar dentro de un string
+const string = 'hello';
+
+console.log(string.padStart(7, '_'));
+
+//metodo padEnd nos permite agregar donde va a terminar y que queremos agregar dentro de un string
+
+console.log(string.padEnd(7, '_'));
---
+++ src/es8/03-trailing-commas.js
@@ -0,0 +1,8 @@
+//actualizacion javascript junio 2017
+
+//trailing commas
+//esto tiene que ver con agregar comas en el array y estas son posiciones que seran creadas en ese array
+const array = [24, 35, 56, , , , , 19];
+
+console.log(array);
+console.log(array.length);
