50951f5763094e04df78c68251978bdfcf5b5750
Reinaldo Romero <reinaldoromero696@gmail.com>
2023-07-19 14:27:41 -0400

    class 11 exponential and array includes es7

---
+++ src/es7/00-exponential.js
@@ -0,0 +1,7 @@
+//actualizacion javascript junio 2016
+
+//exponential
+//nos permite trabajar con este operador (**) para hacer la potencia de un numero
+const data = 3 ** 4;
+
+console.log(data);
---
+++ src/es7/01-array-includes.js
@@ -0,0 +1,11 @@
+//actualizacion javascript junio 2016
+
+//array includes
+//nos permite trabajar sobre un array y saber si esta ahi un elemento
+// con includes validamos si el valos que colocamos esta dentro del array
+let numbers = [1, 2, 3, 4, 5];
+
+console.log(numbers.includes(4));
+
+const list = ['pedro', 'david', 'ana'];
+console.log(list.includes('julian'));
