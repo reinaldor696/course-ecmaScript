dc1db2c877ca1f96ab2622f73a29fc5dcfa402e0
Reinaldo Romero <reinaldoromero696@gmail.com>
2023-07-19 14:10:49 -0400

    class 10 set add es6

---
+++ src/es6/10-set-add.js
@@ -0,0 +1,11 @@
+//actualizacion javascript junio 2015
+
+//set add: creamos una lista y la añadimos al recurso "new set" y a esta lista vamos a agregarles varios item que queremos para luego mostrarlos
+//solo añade nuevos elementos a la sentencia o el objeto set que tenemos
+
+const list = new Set();
+
+list.add('item 1');
+list.add('item 2').add('item 3');
+
+console.log(list);
