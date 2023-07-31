006278a526f324b9e3cfc1a8b6e298d5900ea2b4
Reinaldo Romero <reinaldoromero696@gmail.com>
2023-07-31 12:25:56 -0400

    class 24 numeric separators and replace all es12

---
+++ src/es12/00-numeric-separators.js
@@ -0,0 +1,7 @@
+//actualizacion javascript junio 2021 
+
+//numeric separators
+//los separadores numericos nos ayudan una lectura mas amigable de cifras numericas se usa(_)
+
+const value = 100_000_000_000_000;
+console.log(value);
---
+++ src/es12/01-replaceall.js
@@ -0,0 +1,9 @@
+//actualizacion javascript junio 2021 
+
+//replaceAll
+//ayuda a remplazar un string o palabras
+const string = 'JavaScript es un maravilloso lenguaje de programacion';
+
+const replaceString = string.replace("JavaScript", "TypeScript");
+
+console.log(replaceString);
